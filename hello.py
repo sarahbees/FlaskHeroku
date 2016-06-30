10587acbecaf8863124b304c802d17795641a37e
Sarah Buchanan <sbuchanan@codeschool.com>
2016-06-30 13:40:03 -0400

    Returns jsonify instead of string

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 5
+    resp = jsonify(5)
+    resp.status_code = 200
+    return resp
 
 if __name__ == "__main__":
     app.run()
